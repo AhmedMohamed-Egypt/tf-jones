d22ca0cb0ff639c63c7f063d477611ad713380dd
Ahmed-Mohamed-Egypt <ahmedatyabas@gmail.com>
2025-01-27 08:10:25 +0200

    update

---
+++ src/js/modules/developer/credentials.js
@@ -0,0 +1,8 @@
+(function($, APP) {
+  APP.Dev.Credentials = {
+    logCredentials: function() {
+      // eslint-disable-next-line
+      console.log('threebit.co.uk');
+    },
+  };
+})(jQuery, window.APP);
